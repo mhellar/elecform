6232381edbbe410b729410357ea601544ef5efaa
Mark Hellar <mark.hellar@gmail.com>
2016-07-13 11:57:25 -0700

    code

--- 3/code/ButtonSerial/ButtonSerial.ino
+++ 3/code/ButtonSerial/ButtonSerial.ino
@@ -2,7 +2,7 @@
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
+const int buttonPin = 3;     // the number of the pushbutton pin
 const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
@@ -13,28 +13,22 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-  Serial.print(buttonState);
+  Serial.println(buttonState);
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonState == LOW) {
-    buttonCount++;
-  } 
-  
-  if (buttonCount == 1) {
-    // turn LED on:
     digitalWrite(ledPin, HIGH);
-    buttonCount++;
-  } 
-
-  if(buttonCount == 2){
-    buttonCount = 0;
+  } else {
+    digitalWrite(ledPin, LOW);
   }
+  
+
   
 }
